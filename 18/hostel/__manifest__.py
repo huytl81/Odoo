821f2c8950cd1055ec5250f7abba555c26579019
root <root@192.168.1.14>
2024-11-29 07:00:45 +0700

    hostel module, create hostel model

---
+++ 18/hostel/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hostel Management",
+
+    'summary': "Manage Hostel easily",
+
+    'description': """
+           Efficiently manage the entire residential 
+           facility in the school.", # Supports reStructuredText(RST) 
+           format (description is Deprecated)
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/hostel_security.xml',
+        'security/ir.model.access.csv',
+        'views/hostel.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'web/static/src/xml/**/*',
+        ],
+    },
+}
+
